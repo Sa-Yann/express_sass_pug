0b0e655fc32a4d67207f168aacbcf5688a0f092a
Yann Leblond <knt4xyz@gmail.com>
2021-03-31 13:34:52 +0200

    📝1er Step et 2emeStep : rappeler une variable tableau via index.j et  ensuite via un fichier.json
    J'ai récupéré la variable' infos 'présententdans all_vars.pug / j ai crée la variable mes infos articles dans index.js (la view sur laquelle je travaille)
    puis inserer la variable dans res.render : res.render('index', { title: 'Express', infos: mesInfosArticles });
    ainsi j'ai sécurisé mes datas en les passants dans le document index.js
    2eme step je cree un doc .json ds le dossier json_datas avec toutes les données du tableau [mesInfosarticles] ...
    je commente la variable ds index.js
    Je determine une constante nomFichier equivalent au chemin qui mene au doc .json contenant les informations que je veux envoyer ds la variable infos de page pug en passant par l index js
    fs.readFile  dans la fonction router.get avec en parametre bnomFichier , utf8 et en EN CALL BACK le res.render de index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
-
+const nomFichier = './json_datas/infos_movies.json'
+// On déclare la const monfichier e lui assignant le non du ficier .json contennat les informations en format json de mesInfosArtciles
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  fs.readFile(nomFichier, 'utf8',
+    // en utilisant le utf8 j evite d'utiliser la fonction la methode to string
+    function (err, data) {
+      if (err) {
+        return (console.log('y a une erreur' + err));
+      }
+      // pas besoin de mettre le esle de if 
+      // console.log(data);
+      let mesInfosArticles = JSON.parse(data);
+      // console.log(bufferEnUtf8);
+      res.render('index', { title: 'Express', infos: mesInfosArticles });
+    });
+  // on commnete le let mesInfosArticles =... après avoir créer un dossier infos_movies.json contenant le tableau contenu ds la variable mesInfosArticles
+  let mesInfosArticles = [{ titreTemplateInfo: "Mon premier super film", imageArtclInfo: "images/amon.jpg" }, { titreTemplateInfo: "Mon deuxième super film", imageArtclInfo: "images/viking.jpg" }, { titreTemplateInfo: "Mon troisième super film", imageArtclInfo: "images/gunjan.jpg" }];
+  // toujours redemarer le serveur ctrl + C pour raffraichir les info coté serveur
+  // infos: mesInfosArticles est la nouvelles variable qui est utilisé comme variable infos dans index.pug
+
 });
 
 module.exports = router;
